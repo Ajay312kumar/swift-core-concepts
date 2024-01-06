d5ca7a2a1406dd71a1ccc585bdee23bdb6a552fd
Ajay Kumar <124868129+ajay-kumar-iphtech@users.noreply.github.com>
2024-01-06 11:01:02 +0530

    user-defaults

---
+++ userDefaults/userDefaults.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+let defaults = UserDefaults.standard
+
+defaults.set(10, forKey: "Ten")
+defaults.set(true, forKey: "isSwitchOn")
+defaults.set("learnUserDefaults", forKey: "userDefaults")
+defaults.set(NSData(), forKey: "AppLastOpened")
+let array = [1,2,3,4,5]
+defaults.set(array, forKey: "numberArray")
+let dictionary = ["name": "Ajay"]
+defaults.set(dictionary, forKey: "dictionary")
+
+
+
+
+let number = defaults.integer(forKey: "Ten")
+print(number)
+
+let switchValue = defaults.bool(forKey: "isSwitchOn")
+print(switchValue)
+
+defaults.string(forKey: "userDefaults")
+defaults.object(forKey: "AppLastOpened")
+let myArray = defaults.array(forKey: "numberArray") as! [Int]
+defaults.dictionary(forKey: "dictionary")
+
+
